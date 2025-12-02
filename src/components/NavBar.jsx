5d5c549bc472fe2e27a3ab44c07b0c23bbaa652b
createlsworklappy <justinecastillo7@gmail.com>
2025-12-02 15:54:21 +0900

    feat: create NavBar with links to Home, Shop, and Cart pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,13 @@
-  
+import { NavLink } from "react-router";
+
+function NavBar() {
+  return (
+    <nav>
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/shop"}>Shop</NavLink>
+      <NavLink to={"/cart"}>Cart</NavLink>
+    </nav>
+  )
+}
+
+export default NavBar;
