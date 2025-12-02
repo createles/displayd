77dfad34dc3160a69d62a428755ae03a7f1a63e7
createlsworklappy <justinecastillo7@gmail.com>
2025-12-02 15:55:28 +0900

    refactor(routes): utilize children elements to produce the pages inside of the Outlet component in the Layout component view

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,20 +2,31 @@ import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import CartPage from "./pages/CartPage";
 import ErrorPage from "./pages/ErrorPage";
+import Layout from "./Layout";
 
 const routes = [
   {
     path: "/",
-    element: <HomePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/shop",
-    element: <ShopPage />,
-  },
-  {
-    path: "/cart",
-    element: <CartPage />,
+    element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: "shop",
+        element: <ShopPage/>
+      },
+      {
+        path: "cart",
+        element: <CartPage/>
+      },
+      { 
+        path: "*", 
+        element: <ErrorPage /> 
+      },
+    ],
   },
 ];
 
