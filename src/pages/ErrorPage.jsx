4c02568d9b46c176e027ac7c0bbe12bfd87fae27
createlsworklappy <justinecastillo7@gmail.com>
2025-12-02 15:56:27 +0900

    feat: implement ErrorPage to useRouterError to provide more information on error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,19 @@
+import { useRouteError } from "react-router";
+import { Link } from "react-router";
+
 function ErrorPage() {
+  const error = useRouteError(); // accesses error
+  console.error(error); // logs the error for debugging
+
   return (
-    <div>This page doesn't exist!</div>
+    <div>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occured.</p>
+      <p>
+        <i>{error?.statusText || error?.message || "Page Not Found"}</i>
+      </p>
+      <Link to="/">Go back home</Link>
+    </div>
   )
 }
 
