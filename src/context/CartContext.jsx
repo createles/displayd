034aaf5adb3954a7e617e56ea20f3e6ec2295fb4
createlsworklappy <justinecastillo7@gmail.com>
2025-12-03 15:44:43 +0900

    feat(CartContext): implement CartContext to handle cartItem values and state update methods (add, remove, update)
    feat(main.js): wrap the app inside CartProvider to connect values and methods
    
    misc: ignore fast refresh lint warning

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,60 @@
+import { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  function addToCart(game) {
+    const isItemInCart = cartItems.some((item) => item.id === game.id);
+
+    if (isItemInCart) {
+      setCartItems((prevItems) => 
+        prevItems.map((item) => 
+          item.id === game.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+        )
+      );
+    } else {
+      setCartItems((prevItems) => 
+        [ ...prevItems, { ...game, quantity: 1}]
+      )
+    }
+  }
+
+  function removeFromCart(gameId) {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== gameId)
+    );
+  }
+
+  function updateQuantity(gameId, amount) {
+    setCartItems((prevItems) => {
+      const updatedCart = prevItems.map((item) => {
+        if (item.id === gameId) {
+          return { ...item, quantity: item.quantity + amount };
+        }
+        
+        return item;
+      });
+
+      return updatedCart.filter((item) => item.quantity > 0);
+    });
+  }
+
+  // test log for cart items
+  console.log("Current cart:", cartItems);
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, updateQuantity}}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useShoppingCart() {
+  return useContext(CartContext);
+};
+
