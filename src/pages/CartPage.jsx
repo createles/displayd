c7a0c1decd91c2870f872282b3fa12d89f002e11
createlsworklappy <justinecastillo7@gmail.com>
2025-12-04 12:14:24 +0900

    feat: create base visuals for cart page to display cart contents

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,6 +1,39 @@
+import { Link } from "react-router";
+import { useShoppingCart } from "../context/CartContext";
+import styles from "./CartPage.module.css"
+
 function CartPage() {
+  const { cartItems, removeFromCart, updateQuantity } = useShoppingCart();
+
+  const grandTotal = cartItems.reduce((total, item) => total + (parseFloat(item.price) * item.quantity), 0).toFixed(2);
+
   return (
-    <div>This is the cart page!</div>
+    <div className={styles.cartContainer}>
+      <h1>Your Cart</h1>
+
+      {cartItems.length === 0 
+        ? <div className={styles.emptyCart}>
+          <p>Your cart is empty.</p>
+          <Link to={"/shop"}>Go back to shop</Link>
+          </div>
+        : <div className={styles.cartCheckout}> 
+            <div className={styles.cartList}>
+              {cartItems.map((item) => 
+                <div key={item.id} className={styles.cartItem}>
+                  <p>{item.name}---{item.quantity}---total price: {parseFloat(item.quantity * item.price).toFixed(2)}</p>
+                  <div className={styles.cartBtns}>
+                    <button onClick={() => updateQuantity(item.id, -1)}>-</button>
+                    <button onClick={() => updateQuantity(item.id, 1)}>+</button>
+                    <button onClick={() => removeFromCart(item.id)}>x</button>
+                  </div>
+                </div>
+              )}
+            </div>
+            <p>{grandTotal}</p>
+            <button>Checkout</button>
+          </div>
+        }
+    </div>
   )
 }
 
