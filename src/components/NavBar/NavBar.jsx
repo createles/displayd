eca1348429dd8c5229a4f4f4d545655f41c5b9ac
createlsworklappy <justinecastillo7@gmail.com>
2026-01-19 16:05:49 +0900

    refactor(NavBar): allow clicking logo to go to home

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,47 @@
 import { Link } from "react-router";
 import { useShoppingCart } from "../../context/CartContext";
-import styles from "../NavBar/NavBar.module.css"
+import styles from "../NavBar/NavBar.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { IoGameControllerOutline, IoCartOutline, IoHomeOutline } from "react-icons/io5";
-
+import {
+  IoGameControllerOutline,
+  IoCartOutline,
+  IoHomeOutline,
+} from "react-icons/io5";
 
 function NavBar() {
-  const { cartItems, openCart} = useShoppingCart();
+  const { cartItems, openCart } = useShoppingCart();
 
-  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+  const totalItems = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0,
+  );
 
   return (
     <nav className={styles.nav}>
-      <img src="/src/assets/images/logo/toon-dp-outline-green.svg" alt="displayd logo" srcset="" className={styles.siteLogo} />
+      <Link to="/" className={styles.logoLink}>
+        <img
+          src="/src/assets/images/logo/toon-dp-outline-green.svg"
+          alt="displayd logo"
+          className={styles.siteLogo}
+        />
+      </Link>
       <Link to={"/"} className={styles.navBtn}>
-        <IoHomeOutline/>
+        <IoHomeOutline />
       </Link>
       <Link to={"/shop"} className={styles.navBtn}>
-        <IoGameControllerOutline/>
+        <IoGameControllerOutline />
       </Link>
-      <button onClick={openCart} className={`${styles.navBtn} ${styles.cartBtn}`}>
-        <IoCartOutline/>
+      <button
+        onClick={openCart}
+        className={`${styles.navBtn} ${styles.cartBtn}`}
+      >
+        <IoCartOutline />
         {/* Only show badge if cart is not empty */}
         {totalItems > 0 && <span className={styles.badge}>{totalItems}</span>}
       </button>
-      <SearchBar/>
+      <SearchBar />
     </nav>
-  )
+  );
 }
 
 export default NavBar;
