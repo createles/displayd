ecd91c0863a400d594c7ab22fe2a0895cd821645
createlsworklappy <justinecastillo7@gmail.com>
2026-01-20 15:32:12 +0900

    refactor(GameCard): allow GameCard's image to route to game details page

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -7,23 +7,25 @@ function GameCard({ game }) {
   const { addToCart } = useShoppingCart();
 
   const [imageError, setImageError] = useState(false);
-
+  
   return (
     <div className={styles.card}>
-      {/* provide fallback div if missing background_image or fetching error */}
-      {(!game.background_image || imageError) ? (
-        <div className={styles.imgFallback}>
-          <span>{game.name}</span>
-        </div>
-      ) : (
-        <img 
-        className={styles.cardImg} 
-        src={game.background_image} 
-        alt={game.name}
-        // set image error on fetch error
-        onError={() => setImageError(true)}
-        />
-      )}
+      <Link to={`/shop/${game.id}`}>
+        {/* provide fallback div if missing background_image or fetching error */}
+        {(!game.background_image || imageError) ? (
+          <div className={styles.imgFallback}>
+            <span>{game.name}</span>
+          </div>
+        ) : (
+          <img
+          className={styles.cardImg}
+          src={game.background_image}
+          alt={game.name}
+          // set image error on fetch error
+          onError={() => setImageError(true)}
+          />
+        )}
+      </Link>
       <Link to={`/shop/${game.id}`}>
         <h3 className={styles.gameTitle}>{game.name}</h3>
       </Link>
